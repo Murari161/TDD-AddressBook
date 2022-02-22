9ed353b93e79e0fd27381376786e45f3c30efd0a
unknown <murarijoshua1@gmail.com>
2022-02-22 17:11:55 +0300

    my first commit

---
+++ address.js
@@ -0,0 +1,23 @@
+//
+let Addbook = [
+    {
+        "firstname" : "Joshua",
+        "lastname" : "Murari",
+        "number" : 123456
+    },
+    {
+        "firstname" : "Joshua2",
+        "lastname" : "Murari2",
+        "number" : 123456
+    },
+    {
+        "firstname" : "Joshua3",
+        "lastname" : "Murari3",
+        "number" : 123456
+    }
+]
+console.log(Addbook[0]["firstname"])
+
+
+
+module.exports = Addbook;
