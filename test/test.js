9756b11ea840b7d9eac9fba7ef72f69adff503fb
unknown <murarijoshua1@gmail.com>
2022-02-26 23:10:54 +0300

    second working test for properties of the array

--- test/test.js
+++ test/test.js
@@ -15,11 +15,18 @@ describe('Tests on address book', function(){
     it('Test of reference to keys', function(){
         expect(Addbook).to.have.any.keys("0", "1", "2");
     });
-    it('Test for properties', function(){
-        expect(Addbook).to.have.a.property('{
+    // it('Test for properties', function(){
+    //     expect(Addbook).to.include({
+    //         "firstname" : "Joshua",
+    //         "lastname" : "Murari",
+    //         "number" : 123456
+    // });
+    it('Test for properties2', function(){
+        expect(Addbook).to.deep.include({
             "firstname" : "Joshua",
             "lastname" : "Murari",
             "number" : 123456
-        }');
+    });
+    
     });
 });
